84df84ca2c293d4a0a4da52cc139d8ea8f19761a
wagymaniac <154833867+wagymaniac@users.noreply.github.com>
2026-05-02 20:29:08 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 const CACHE = 'migym-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const FILES = [
+  '/mi-gym/',
+  '/mi-gym/index.html',
+  '/mi-gym/manifest.json'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
@@ -15,6 +19,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/mi-gym/index.html')))
   );
 });
